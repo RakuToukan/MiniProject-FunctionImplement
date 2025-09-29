d92a872de97a4635b01526fdb0d16dff0d50d8bd
Raku Toukan <thespongetrolls@gmail.com>
2025-09-29 10:31:21 +0700

    first commit

---
+++ tebakAngka.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    // Mengambil angka rahasia secara acak antara 1 dan 100
+    int angkaRahasia = rand() % 101;
+    int tebakan;
+    int percobaan = 0;
+
+    cout << "=== Tebak Angka Random ===" << endl;
+    cout << "Saya sudah memilih angka antara 1 dan 100. Coba tebak!" << endl;
+
+    do {
+        cout << "Masukkan tebakan Anda: ";
+        cin >> tebakan;
+        percobaan++;
+
+        // Memeriksa apakah input valid
+        if (cin.fail() || tebakan < 1 || tebakan > 100) {
+            cout << "Masukan tidak valid. Masukkan bilangan bulat antara 1 dan 100." << endl;
+            // Membersihkan buffer input yang error
+            cin.clear();
+            cin.ignore(1000, '\n');
+            percobaan--; // Mengurangi hitungan percobaan karena input salah
+            continue;
+        }
+
+        // Memberikan petunjuk
+        if (tebakan > angkaRahasia) {
+            cout << "Terlalu tinggi! Coba lagi." << endl;
+        } else if (tebakan < angkaRahasia) {
+            cout << "Terlalu rendah! Coba lagi." << endl;
+        } else {
+            cout << "Selamat! Anda menebak dengan benar." << endl;
+            cout << "Anda berhasil dalam " << percobaan << " kali percobaan." << endl;
+        }
+    } while (tebakan != angkaRahasia);
+
+    return 0;
+}
